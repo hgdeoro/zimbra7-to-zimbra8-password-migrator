6539eb0449f1da6f367d1313c3b34946fd5e4167
Horacio G. de Oro <hgdeoro@gmail.com>
2015-03-27 16:42:44 -0300

    Remove doc from the module.
    That doc is in the README now

--- generate_ldap_diff.py
+++ generate_ldap_diff.py
@@ -4,52 +4,14 @@ from __future__ import unicode_literals
 
 import base64
 import fileinput
-import pprint
 import re
 
 
-"""
-
-# Based on:
-#  - http://wiki.zimbra.com/wiki/ShanxT-LDAP-CheatSheet
-#  - http://wiki.zimbra.com/wiki/Setting_zimbra_admin_password_in_LDAP
-#  - http://www.openldap.org/faq/data/cache/347.html
-#  - http://wiki.zimbra.com/wiki/Integrating_PWM_password_manager_with_Zimbra
-#  - https://wiki.zimbra.com/wiki/UNIX_and_Windows_Accounts_in_Zimbra_LDAP_and_Zimbra_Admin_UI
-#  - http://www.math.ucla.edu/~jimc/documents/pimstuff/zimbra.txt
-
-
-On Zimbra 7 host, execute:
-
-    root@zimbra7~$ sudo -u zimbra -i
-    zimbra@zimbra7~$ source ~/bin/zmshutil
-    zimbra@zimbra7~$ zmsetvars
-
-    zimbra@zimbra7~$ ldapsearch -o ldif-wrap=no -x -H $ldap_master_url \
-        -D $zimbra_ldap_userdn -w $zimbra_ldap_password -LLL \
-        '(&(objectClass=zimbraAccount)(ou:dn:=people))' \
-        dn displayName zimbraMailStatus zimbraMailDeliveryAddress \
-        mail userPassword > /dev/shm/dump-zimbra-7.ldif
-
-    zimbra@zimbra7~$ python generate_ldap_diff.py /dev/shm/dump-zimbra-7.ldif > /dev/shm/dump-zimbra-8.ldif 
-
-    zimbra@zimbra7~$ scp /dev/shm/dump-zimbra-8.ldif user@zimbra8:/dev/shm
-
-On Zimbra 8 host, execute:
-
-    root@zimbra8~$ sudo -u zimbra -i
-    zimbra@zimbra8~$ source ~/bin/zmshutil
-    zimbra@zimbra8~$ zmsetvars
-
-    zimbra@zimbra8~$ ldapmodify -v -x \
-        -H $ldap_master_url \
-        -D $zimbra_ldap_userdn \
-        -w $zimbra_ldap_password -c -f /dev/shm/dump-zimbra-8.ldif
-
-
-"""
-
 SPLITTER = re.compile(r'\s*:+\s*')
+
+"""
+For information and instructions, see the README.md file
+"""
 
 
 def generate_dict(current_object_lines):
