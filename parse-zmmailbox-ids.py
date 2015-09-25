e8576ff0602d277dd689a40150a35f14ef2dd1d5
Horacio G. de Oro <hgdeoro@gmail.com>
2015-09-25 11:34:42 -0300

    Works with empty results, veirfy result count

--- parse-zmmailbox-ids.py
+++ parse-zmmailbox-ids.py
@@ -9,6 +9,13 @@ import sys
 # 1. -946182  conv   admin                 Daily mail report                                   09/24/15 23:57
 # 2.  421345  conv   John                  Some great news for you                             09/24/15 23:57
 
+# $ zmmailbox -z -m username@domain.tld search -l 200 "in:/inbox (before:today)"
+# num: 0, more: false
+#
+
+
+REGEX_RESULT_STATUS = re.compile(r'^num: (\d+), more: (\S+)$')
+
 REGEX_HEAD = re.compile(r'^Id')
 REGEX_HEAD_SEP = re.compile(r'^---')
 
@@ -17,6 +24,12 @@ REGEX_DATA = re.compile(r'^(\d+)\.\s+(\-?\d+)\s+(\S+)')
 
 def main():
     lines = [line.strip() for line in sys.stdin.readlines() if line.strip()]
+
+    status_count, status_more = REGEX_RESULT_STATUS.match(lines.pop(0)).groups()
+    status_count = int(status_count)
+
+    if status_count == 0:
+        return
 
     while True:
         line = lines.pop(0)
@@ -35,6 +48,8 @@ def main():
         else:
             sys.stderr.write("Couldn't parse line: {0}\n".format(line))
             sys.exit(1)
+
+    assert len(ids) == status_count
 
     print ','.join(ids)
 
